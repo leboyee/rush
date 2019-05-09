24d48cb8ef1d9318f3fa8e00c68e7808ec35061e
iChirag <ideveloper2@iChirag.local>
2019-05-09 18:05:39 +0530

    addTabbarUI

---
+++ Rush/Classes/Modules/CustomTabbarViewController.swift
@@ -0,0 +1,119 @@
+//
+//  TabbarViewController.swift
+//  Rush
+//
+//  Created by ideveloper2 on 09/05/19.
+//  Copyright © 2019 Messapps. All rights reserved.
+//
+
+import UIKit
+
+let globalAppBgColor = UIColor.white
+let tabbarItemTintColor = UIColor(red: 0.56, green: 0.24, blue: 0.33, alpha: 1)
+
+
+class CustomTabbarViewController: UITabBarController, UITabBarControllerDelegate {
+    
+    var firstNavigationController       : UINavigationController!
+    var secondNavigationController      : UINavigationController!
+    var thirdNavigationController       : UINavigationController!
+    var fourthNavigationViewController  : UINavigationController!
+    var fifthNavigationController       : UINavigationController!
+    
+    let userImageName = "profile_tab_active"
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        setupUI()
+        setupTabbar()
+    }
+}
+
+// MARK: - Other functions
+extension CustomTabbarViewController {
+    
+    func setupUI() {
+        
+        tabBar.barTintColor = globalAppBgColor
+        tabBar.tintColor = tabbarItemTintColor
+        
+        let fStory = UIStoryboard(name: "Home", bundle: nil)
+        let homeViewController = fStory.instantiateInitialViewController()
+        firstNavigationController = UINavigationController(rootViewController: homeViewController!)
+        
+        let sStory = UIStoryboard(name: "Explore", bundle: nil)
+        let exploreViewController = sStory.instantiateInitialViewController()
+        secondNavigationController = UINavigationController(rootViewController: exploreViewController!)
+        
+        let tStory = UIStoryboard(name: "Chats", bundle: nil)
+        let forumViewController = tStory.instantiateInitialViewController()
+        thirdNavigationController = UINavigationController(rootViewController: forumViewController!)
+        
+        let chatStory = UIStoryboard(name: "Forum", bundle: nil)
+        let chatsViewController = chatStory.instantiateInitialViewController()
+        fourthNavigationViewController = UINavigationController(rootViewController: chatsViewController!)
+        
+        let profileStory = UIStoryboard(name: "Profile", bundle: nil)
+        let profileViewController = profileStory.instantiateInitialViewController()
+        fifthNavigationController = UINavigationController(rootViewController: profileViewController!)
+        
+        self.delegate = self
+        setupIcons()
+    }
+    
+    func setupIcons() {
+        
+        firstNavigationController.tabBarItem.image = UIImage(named: "home_tab_inactive")
+        firstNavigationController.tabBarItem.selectedImage =
+            UIImage(named: "home_tab_active")
+        
+        secondNavigationController.tabBarItem.image = UIImage(named: "explore_tab_inactive")
+        secondNavigationController.tabBarItem.selectedImage = UIImage(named: "explore_tab_active")
+        
+        thirdNavigationController.tabBarItem.image = UIImage(named: "forum_tab_inactive")
+        thirdNavigationController.tabBarItem.selectedImage = UIImage(named: "forum_tab_active")
+        
+        fourthNavigationViewController.tabBarItem.image = UIImage(named: "chat_tab_inactive")
+        fourthNavigationViewController.tabBarItem.selectedImage = UIImage(named: "chat_tab_active")
+        
+        
+        let customTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(named: "profile_tab_inactive")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "profile_tab_active")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal))
+        fifthNavigationController.tabBarItem = customTabBarItem
+    }
+    
+    func setupTabbar() {
+        
+        viewControllers = [firstNavigationController, secondNavigationController, thirdNavigationController, fourthNavigationViewController, fifthNavigationController]
+        
+        /*
+        let profileImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 23, height: 23))
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = 11.5
+        profileImageView.image = UIImage(named: userImageName)
+        */
+        
+        for tabBarItem in tabBar.items! {
+            tabBarItem.title = ""
+            tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        }
+    }
+}
+
+// MARK: - Tabbar delegate
+extension CustomTabbarViewController {
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+
+    }
+}
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
