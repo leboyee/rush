9eb861131bf847222aa585275e0db3f5ad050c80
SureshMessapps <sj@messapps.com>
2019-05-13 13:16:48 +0530

    - working on boarding

---
+++ Rush/Classes/CustomClass/CustomOnboardImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomOnboardImageView.swift
+//  Rush
+//
+//  Created by Suresh Jagnani on 13/05/19.
+//  Copyright © 2019 Messapps. All rights reserved.
+//
+
+import UIKit
+
+class CustomOnboardImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
