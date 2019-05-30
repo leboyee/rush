984a5afbed05f56f05ffac1fffaf30684ceede39
SureshMessapps <sj@messapps.com>
2019-05-30 16:26:02 +0530

    - add country code

---
+++ Rush/Classes/Modules/CustomPickers/CustomPicker/Mediator/CustomPickerMediator.swift
@@ -0,0 +1,54 @@
+//
+//  CustomPickerMediator.swift
+//  wolf
+//
+//  Created by Suresh Jagnani on 02/07/18.
+//  Copyright © 2018 Suresh Jagnani. All rights reserved.
+//
+
+import UIKit
+
+class CustomPickerMediator: NSObject {
+
+    weak var pickerView : UIPickerView?
+    var selectedRow: Int = 0
+
+    var numberOfComponents: (() -> Int)?
+    var numberOfRowsInComponent: ((_ component : Int) -> Int)?
+    var fillPicker: ((_ component : Int, _ row : Int) -> String)?
+    var selected: ((_ component : Int, _ row : Int) -> Void)?
+    
+    //MARK: - Life Cycle
+    deinit {
+        pickerView = nil
+    }
+    
+    //MARK: - Other Functions
+    func setupMediator(pickerView: UIPickerView) {
+        self.pickerView = pickerView
+        self.pickerView?.delegate = self
+        self.pickerView?.dataSource = self
+    }
+}
+
+extension CustomPickerMediator: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return numberOfComponents?() ?? 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return numberOfRowsInComponent?(component) ?? 0
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fillPicker?(component, row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selected?(component, row)
+    }
+    
+}
---
+++ Rush/Classes/Modules/CustomPickers/CustomPicker/ViewLayer/CustomPickerViewController.swift
@@ -0,0 +1,116 @@
+
+//
+//  CustomPickerViewController.swift
+//  wolf
+//
+//  Created by Suresh Jagnani on 02/07/18.
+//  Copyright © 2018 Suresh Jagnani. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol CustomPickerDelegate {
+    
+    // Picker
+    @objc func selectedValue(data: String, type: String)
+    @objc optional func selectedCountryValue(countryName: String, countryCode: String)
+
+}
+
+class CustomPickerViewController: UIViewController {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var containerViewConstraint: NSLayoutConstraint!
+
+    var selectedData = ""
+    var dataArray = [String]()
+    var presenter = CustomPickerPreseneter()
+    var mediator  = CustomPickerMediator()
+    weak var pickerDelegate : CustomPickerDelegate!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setup()
+        setupMediator()
+        setupPresenter()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.containerViewConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.layoutIfNeeded()
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // MARK: - setup
+    func setup() {
+        
+    }
+    
+    func setupMediator() {
+        mediator.setupMediator(pickerView: pickerView)
+        setupMediatorHandlers()
+        
+    }
+    
+    func setupPresenter() {
+        presenter.mediator = mediator
+        setupPresenterHandlers()
+        presenter.viewIsReady()
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func doneButtonAction() {
+        if presenter.selectedValue == "" {
+            presenter.selectedValue = presenter.pickerDataList![0]
+        }
+        if presenter.type == .country {
+            var countryCodeValue = ""
+            if let categoryId = presenter.countryCode.index(where: { ($0["name"] as! String) == presenter.selectedValue }) {
+                countryCodeValue = presenter.countryCode[categoryId]["dial_code"] as! String
+            }
+            
+            pickerDelegate?.selectedCountryValue!(countryName: presenter.selectedValue, countryCode: countryCodeValue)
+        } else {
+            pickerDelegate.selectedValue(data: presenter.selectedValue, type: presenter.type.rawValue)
+        }
+        dismiss(animated: false, completion: nil)
+    }
+    
+    @IBAction func dismissButtonAction(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+}
+
+//MARK: - Input View Handler (Or Presenter Output)
+extension CustomPickerViewController {
+    func setupPresenterHandlers() {
+        presenter.updateTitle = { [weak self] (title) in
+            guard let self_ = self else { return }
+            self_.titleLabel.text = title
+        }
+    }
+}
+
+//MARK: - Input View Handler (Or Mediator Output)
+extension CustomPickerViewController {
+    
+    func setupMediatorHandlers() {
+        
+    }
+}
+
