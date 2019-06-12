612bc778c9952b17a51e9181d6638e7864d576af
iChirag <ichirag.lukhi@gmail.com>
2019-06-12 20:12:43 +0530

    set image in  after choose image.

--- Rush/Classes/Modules/Tabbar/Home/CreatePost/CreatePostMediates.swift
+++ Rush/Classes/Modules/Tabbar/Home/CreatePost/CreatePostMediates.swift
@@ -21,26 +21,28 @@ extension CreatePostViewController :UITableViewDelegate,UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         if imageList.count > 0 {
             return 3
-        }else {
+        } else {
             return 2
         }
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 2 {
+            return imageList.count
+        }
         return 1
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: Cell.userName, for: indexPath) as! UserNameTableViewCell
             return cell
-        }else if  indexPath.section == 1 {
+        } else if indexPath.section == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: Cell.userPostText, for: indexPath) as! UserPostTextTableViewCell
             return cell
-        }else {
+        } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: Cell.userPostImage, for: indexPath) as! UserPostImageTableViewCell
+            cell.postImageView.image = imageList[indexPath.row]
             return cell
         }
     }
@@ -51,7 +53,19 @@ extension CreatePostViewController :UITableViewDelegate,UITableViewDataSource {
     }
     
 }
+//MARK:- Action
 
 extension CreatePostViewController :UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            imageList.append(pickedImage)
+        }
+        picker.dismiss(animated: true, completion: nil)
+        tableView.reloadData()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
