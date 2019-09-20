85155077b601877fd6623ba27b2cdc41f40fe1fc
SureshMessapps <sj@messapps.com>
2019-09-20 17:33:05 +0530

    fb share

--- Rush/Classes/Modules/UserInfo/AddInvite/AddInviteViewLogics.swift
+++ Rush/Classes/Modules/UserInfo/AddInvite/AddInviteViewLogics.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import FacebookShare
+import FacebookCore
+
 extension AddInviteViewController {
 }
 
 // MARK: - Manage Interator or API's Calling
 extension AddInviteViewController {
+    
+}
+
+extension AddInviteViewController: SharingDelegate {
+    func sharer(_ sharer: Sharing, didCompleteWithResults results: [String: Any]) {
+        
+    }
+    
+    func sharer(_ sharer: Sharing, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func sharerDidCancel(_ sharer: Sharing) {
+        
+    }
 }
