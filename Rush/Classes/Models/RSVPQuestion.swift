ae104f201deed536508ee519df7d5c3730317737
Kamal Mittal <info.woodenlabs@gmail.com>
2019-10-01 12:11:11 +0530

    Working on RSVP

---
+++ Rush/Classes/Models/RSVPQuestion.swift
@@ -0,0 +1,22 @@
+//
+//  RSVPQuestion.swift
+//  Rush
+//
+//  Created by kamal on 01/10/19.
+//  Copyright © 2019 Messapps. All rights reserved.
+//
+
+import UIKit
+class RSVPQuestion: Codable {
+    var index: Int
+    var que: String?
+    var ans: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case index
+        case que
+        case ans
+
+    }
+    
+}
