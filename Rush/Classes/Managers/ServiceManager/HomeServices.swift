44c9e5790a47f06392c9054f7cc93f6898aae21e
iChirag <ichirag.lukhi@gmail.com>
2019-10-01 12:34:18 +0530

    pushed changes about codable api

--- Rush/Classes/Managers/ServiceManager/HomeServices.swift
+++ Rush/Classes/Managers/ServiceManager/HomeServices.swift
@@ -55,46 +55,4 @@ extension ServiceManager {
             })
         }
     }
-    
-    func postComment(params: [String: Any], closer: @escaping (_ status: Bool, _ errorMessage: String?) -> Void) {
-        NetworkManager.shared.postComment(param: params) { [weak self] (data, error, code) in
-            guard let uwself = self else { return }
-            uwself.processNoDataResponse(result: data, error: error, code: code, closer: { (status, errorMessage) in
-                closer(status, errorMessage)
-            })
-        }
-    }
-    
-    func fetchCommentList(postId: String, closer: @escaping (_ comments: [Comment]?, _ errorMessage: String?) -> Void) {
-        NetworkManager.shared.getCommentList(postId: postId, param: [:]) { [weak self] (data, error, code) in
-            guard let uwself = self else { return }
-            uwself.procesModelResponse(result: data, error: error, code: code, closer: { (comments, errorMessage) in
-                closer(comments, errorMessage)
-            })
-        }
-    }
-        
-    func votePost(postId: String, voteType: String, closer: @escaping (_ post: Post?, _ errorMessage: String?) -> Void) {
-        NetworkManager.shared.votePost(postId: postId, voteType: voteType) { [weak self] (data, error, code) in
-            guard let uwself = self else { return }
-            uwself.processDataResponse(result: data, error: error, code: code, closer: { (data, errorMessage) in
-                if let object = data?[Keys.post] as? [String: Any] {
-                    let post: Post? = uwself.decodeObject(fromData: object)
-                    closer(post, nil)
-                } else {
-                    closer(nil, errorMessage)
-                }
-            })
-        }
-    }
-    
-    func getPostList(dataId: String, type: String, params: [String: Any], closer: @escaping (_ params: [Post]?, _ errorMessage: String?) -> Void) {
-        NetworkManager.shared.fetchPostList(dataId: dataId, type: type, params: params) { [weak self] (data, error, code) in
-            guard let uwself = self else { return }
-            uwself.procesModelResponse(result: data, error: error, code: code, closer: { (posts, errorMessage) in
-                 closer(posts, errorMessage)
-            })
-        }
-    }
-
 }
