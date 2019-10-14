9cbe627c350e7aaadffa460a94f2770bb0acda0e
SureshMessapps <sj@messapps.com>
2019-10-14 12:38:49 +0530

    working on user profile image

--- Rush/Classes/Modules/Tabbar/Profile/ProfileTileViewController/ProfileTileMediates.swift
+++ Rush/Classes/Modules/Tabbar/Profile/ProfileTileViewController/ProfileTileMediates.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-extension ProfileTileViewController : UICollectionViewDelegate,UICollectionViewDataSource
-{
+extension ProfileTileViewController : UICollectionViewDelegate, UICollectionViewDataSource {
     
      func setupCollectionView() {
         collectionView.delegate = self
@@ -19,11 +18,10 @@ extension ProfileTileViewController : UICollectionViewDelegate,UICollectionViewD
         layout.scrollDirection = .vertical
         collectionView.collectionViewLayout = layout
         collectionView.reloadData()
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
+        return 0
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -37,20 +35,21 @@ extension ProfileTileViewController : UICollectionViewDelegate,UICollectionViewD
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.profileTileCell, for: indexPath) as? ProfileTileCell else { return UICollectionViewCell() }
-        
-        fillCell(cell,indexPath)
-       
+        fillCell(cell, indexPath)
         cell.layoutIfNeeded()
         return cell
     }
-    
-    
 }
 
-extension ProfileTileViewController : UICollectionViewDelegateFlowLayout
-{
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-    {
+extension ProfileTileViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return cellHeight()
     }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
--- Rush/Classes/Modules/Tabbar/Profile/ProfileTileViewController/ProfileTileViewController.swift
+++ Rush/Classes/Modules/Tabbar/Profile/ProfileTileViewController/ProfileTileViewController.swift
@@ -16,7 +16,7 @@ class ProfileTileViewController: UIViewController {
     @IBOutlet weak var titleLable: UILabel!
 
     let layout = UICollectionViewFlowLayout()
-    var selectedImage : UIImage? = UIImage(contentsOfFile: "")
+    var selectedImage: UIImage? = UIImage(contentsOfFile: "")
     
     var imageArray =  ["https://tineye.com/images/widgets/mona.jpg","https://cdn1.epicgames.com/ue/product/Screenshot/UE4Editor2019-01-1606-56-33-1920x1080-b727b2460d08c22d7d4a6d5b3e8ca4d4.jpg","https://cdn1.epicgames.com/ue/product/Screenshot/UE4Editor2019-01-1606-56-33-1920x1080-b727b2460d08c22d7d4a6d5b3e8ca4d4.jpg","https://tineye.com/images/widgets/mona.jpg","https://tineye.com/images/widgets/mona.jpg","https://tineye.com/images/widgets/mona.jpg","https://tineye.com/images/widgets/mona.jpg","https://tineye.com/images/widgets/mona.jpg","https://cdn1.epicgames.com/ue/product/Screenshot/UE4Editor2019-01-1606-56-33-1920x1080-b727b2460d08c22d7d4a6d5b3e8ca4d4.jpg"]
     
@@ -27,23 +27,19 @@ class ProfileTileViewController: UIViewController {
     
     func setupUI() {
         self.view.backgroundColor = UIColor.bgBlack
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "back-arrow"), style: .plain, target: self, action: #selector(addButtonAction))
         
         // Setup tableview
         setupCollectionView()
     }
-    
-    
-     // MARK:- Actions
+      // MARK: - Actions
     
     @IBAction func backButtonAction(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
-    
-    //MARK:- Setup Title
-    
-    func setTitle(titleStr:String) {
-        titleLable.text=titleStr
+ 
+    // MARK: - Setup Title
+    func setTitle(titleStr: String) {
+        titleLable.text = titleStr
     }
     
 }
