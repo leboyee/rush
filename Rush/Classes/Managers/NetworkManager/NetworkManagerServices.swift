b4202d7dbc0c2aaa1ce1687bed5871da110981ea
iChirag <ichirag.lukhi@gmail.com>
2019-10-16 18:34:06 +0530

    my joined classes displayed

--- Rush/Classes/Managers/NetworkManager/NetworkManagerServices.swift
+++ Rush/Classes/Managers/NetworkManager/NetworkManagerServices.swift
@@ -206,6 +206,9 @@ extension NetworkManager {
     func getClassList(params: [String: Any], resultHandler: @escaping ResultClosure) {
         requestGet(path: "class/list", params: params, resultHandler: resultHandler)
     }
+    func getMyJoinedClassList(params: [String: Any], resultHandler: @escaping ResultClosure) {
+        requestGet(path: "profile/class/list", params: params, resultHandler: resultHandler)
+    }
 
     func getClassGroupList(classId: String, params: [String: Any], resultHandler: @escaping ResultClosure) {
         requestGet(path: "class/\(classId)/groups", params: params, resultHandler: resultHandler)
