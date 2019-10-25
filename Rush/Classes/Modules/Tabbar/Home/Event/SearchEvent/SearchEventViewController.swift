b8c4ecdb8662914db194d30674514d7bc859d707
iChirag <ichirag.lukhi@gmail.com>
2019-10-25 13:35:11 +0530

    add searching in chat contact list screen

--- Rush/Classes/Modules/Tabbar/Home/Event/SearchEvent/SearchEventViewController.swift
+++ Rush/Classes/Modules/Tabbar/Home/Event/SearchEvent/SearchEventViewController.swift
@@ -13,7 +13,7 @@ class SearchEventViewController: CustomViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var noEventsView: UIView!
-
+    
     var selectedIndex = -1
     var searchText = ""
     var pageNo = 1
@@ -34,9 +34,9 @@ class SearchEventViewController: CustomViewController {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-         navigationController?.navigationBar.backgroundColor = UIColor.clear
-         navigationController?.navigationBar.barTintColor = UIColor.clear
-     }
+        navigationController?.navigationBar.backgroundColor = UIColor.clear
+        navigationController?.navigationBar.barTintColor = UIColor.clear
+    }
     
     func setup() {
         setupUI()
@@ -54,19 +54,19 @@ class SearchEventViewController: CustomViewController {
         // Set left bar button and title
         let customView = UIView(frame: CGRect(x: 48, y: 0, width: screenWidth - 48, height: 44))
         
-            let searchTextField = UITextField(frame: CGRect(x: 0, y: -3, width: screenWidth - 48, height: 44))
-            searchTextField.font = UIFont.displayBold(sz: 24)
-            searchTextField.textColor = UIColor.white
-            searchTextField.returnKeyType = .go
-            searchTextField.autocorrectionType = .no
-            searchTextField.delegate = self
-            let font = UIFont.displayBold(sz: 24)
-            let color = UIColor.navBarTitleWhite32
-            searchTextField.attributedPlaceholder = NSAttributedString(string: "Search events", attributes: [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: color])
-            searchTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
-            customView.addSubview(searchTextField)
-            navigationItem.titleView = customView
-        }
+        let searchTextField = UITextField(frame: CGRect(x: 0, y: -3, width: screenWidth - 48, height: 44))
+        searchTextField.font = UIFont.displayBold(sz: 24)
+        searchTextField.textColor = UIColor.white
+        searchTextField.returnKeyType = .go
+        searchTextField.autocorrectionType = .no
+        searchTextField.delegate = self
+        let font = UIFont.displayBold(sz: 24)
+        let color = UIColor.navBarTitleWhite32
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Search events", attributes: [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: color])
+        searchTextField.addTarget(self, action: #selector(textDidChange(_:)), for: .editingChanged)
+        customView.addSubview(searchTextField)
+        navigationItem.titleView = customView
+    }
 }
 
 // MARK: - Actions
