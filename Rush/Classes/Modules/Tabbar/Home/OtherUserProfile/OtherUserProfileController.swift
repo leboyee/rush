66d6120601fb8ccb081d549b201680f0ce9af3fe
SureshMessapps <sj@messapps.com>
2019-11-04 21:13:58 +0530

    resolved error

--- Rush/Classes/Modules/Tabbar/Home/OtherUserProfile/OtherUserProfileController.swift
+++ Rush/Classes/Modules/Tabbar/Home/OtherUserProfile/OtherUserProfileController.swift
@@ -67,13 +67,12 @@ class OtherUserProfileController: UIViewController {
     }
     
     func setupUI() {
-        topConstraintOfTableView.constant = -Utils.navigationHeigh        
         // share button
         let share = UIBarButtonItem(image: #imageLiteral(resourceName: "share"), style: .plain, target: self, action: #selector(shareButtonAction))
         navigationItem.rightBarButtonItem = share
         
         // back button
-        let cancel = UIBarButtonItem(image: #imageLiteral(resourceName: "back-arrow"), style: .plain, target: self, action: #selector(cancelButtonAction))
+        let cancel = UIBarButtonItem(image: #imageLiteral(resourceName: "back-arrow"), style: .plain, target: self, action: #selector(backButtonAction))
         navigationItem.leftBarButtonItem = cancel
 
         setupTableView()
