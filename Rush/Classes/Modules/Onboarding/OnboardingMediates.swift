a65070ea19940d6a7d30c96ad8be4214e6a4d370
SureshMessapps <sj@messapps.com>
2019-11-05 17:31:02 +0530

    on boarding done

--- Rush/Classes/Modules/Onboarding/OnboardingMediates.swift
+++ Rush/Classes/Modules/Onboarding/OnboardingMediates.swift
@@ -25,7 +25,7 @@ extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDa
         return 1
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
