18f8cd239ff384ae9fc0369a502034ea86ac0d51
Kamal Mittal <info.woodenlabs@gmail.com>
2020-01-17 18:12:14 +0530

    Added Custom Print function

--- Rush/Miscellaneous/Utils.swift
+++ Rush/Miscellaneous/Utils.swift
@@ -565,3 +565,21 @@ extension Utils {
         controller.present(activityViewController, animated: true, completion: nil)
     }
 }
+
+// MARK: - Global functions
+func print(_ item: @autoclosure () -> Any, separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+    Swift.print(item(), separator: separator, terminator: terminator)
+    #endif
+}
+
+func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+    var idx = items.startIndex
+    let endIdx = items.endIndex
+    repeat {
+        Swift.print(items[idx], separator: separator, terminator: idx == (endIdx - 1) ? terminator : separator)
+        idx += 1
+    } while idx < endIdx
+    #endif
+}
